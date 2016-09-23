88a235adbd4b282c624f3e7140920f91875493ec
David Luo <davidl2381@gmail.com>
2016-09-22 20:34:42 -0400

    Added setname and setavatar commands

--- discord_bot.py
+++ discord_bot.py
@@ -1,8 +1,8 @@
 import json
 import logging
-import sys
-import traceback
 
+import discord
+import requests
 from discord.ext import commands
 
 logging.basicConfig(format="[%(asctime)s] [%(levelname)-8s] - %(message)s", level=logging.INFO)
@@ -17,30 +17,53 @@ def load_config():
         return json.load(f)
 
 
-@bot.event
-async def on_command_error(error, ctx):
-    await bot.send_message(ctx.message.channel, "Something went wrong. Probably a syntax error.")
-    print('In {0.command.qualified_name}:'.format(ctx), file=sys.stderr)
-    traceback.print_tb(error.original.__traceback__)
-    print('{0.__class__.__name__}: {0}'.format(error.original), file=sys.stderr)
+# @bot.event
+# async def on_command_error(error, ctx):
+#     await bot.send_message(ctx.message.channel, "Something went wrong. Probably a syntax error.")
+#     print('In {0.command.qualified_name}:'.format(ctx), file=sys.stderr)
+#     traceback.print_tb(error.original.__traceback__)
+#     print('{0.__class__.__name__}: {0}'.format(error.original), file=sys.stderr)
 
-# @commands.command(
-#     name="setname",
-#     hidden=True
-# )
-# async def set_name(name):
 
+@bot.command(
+    name="setname",
+    hidden=True,
+    pass_context=True
+)
+async def set_name(ctx, *, name):
+    if ctx.message.author.id == bot.config['discord']['owner_client_id']:
+        await bot.change_nickname(ctx.message.server.me, name)
+
+        await bot.say("Bot name set to " + name + ".")
+    else:
+        await bot.say("You are not the bot owner!")
+
+
+@bot.command(
+    name="setavatar",
+    hidden=True,
+    pass_context=True
+)
+async def set_avatar(ctx, image_url):
+    if ctx.message.author.id == bot.config['discord']['owner_client_id']:
+        try:
+            r = requests.get(image_url)
+
+            await bot.edit_profile(avatar=r.content)
+            await bot.say("Bot avatar set.")
+        except requests.exceptions.MissingSchema:
+            await bot.say("Invalid URL.")
+        except discord.errors.InvalidArgument:
+            await bot.say("Not a valid avatar.")
 
 
 if __name__ == '__main__':
-    config = load_config()
+    bot.config = load_config()
 
     extensions = [
         'cogs.warcraftlogs',
         'cogs.rnjesus'
     ]
-
-    bot.config = config
 
     for extension in extensions:
         try:
@@ -49,4 +72,4 @@ if __name__ == '__main__':
         except Exception as e:
             logging.error('Failed to load extension {}\n{}: {}'.format(extension, type(e).__name__, e))
 
-    bot.run(config['discord']['discord_token'])
+    bot.run(bot.config['discord']['discord_token'])
