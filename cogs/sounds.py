11a645c76e5aa98de906f5b85e1c6be40d056848
David Luo <davidl2381@gmail.com>
2016-09-26 19:59:20 -0400

    Merely a setback.

--- cogs/sounds.py
+++ cogs/sounds.py
@@ -45,6 +45,28 @@ class Sounds:
 
             player = client.create_ffmpeg_player('sounds/prepared.mp3')
 
+            player.volume = .30
+            player.start()
+
+            player.join()
+            await client.disconnect()
+
+    @commands.command(
+        name="setback",
+        # alias=["setback"],
+        description="TEMPEST KEEP WAS MERELY A SETBACK",
+        brief="MERELY A SETBACK",
+        pass_context=True,
+        no_pm=True
+    )
+    async def setback(self, ctx):
+        v_channel = ctx.message.author.voice
+
+        if v_channel:
+            client = await self.bot.join_voice_channel(v_channel.voice_channel)
+
+            player = client.create_ffmpeg_player('sounds/setback.mp3')
+
             player.volume = .40
             player.start()
 
