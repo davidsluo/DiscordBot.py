1ef92e0d7b29fd2d495f3d506d786053ac72d37d
David Luo <davidl2381@gmail.com>
2016-09-28 23:58:42 -0400

    Bot message cleanup command.

--- cogs/core.py
+++ cogs/core.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 class Core:
     def __init__(self, bot):
         self.bot = bot
-        
+
     @commands.command(
         name="setname",
         hidden=True,
@@ -26,7 +26,7 @@ class Core:
         pass_context=True
     )
     async def set_avatar(self, ctx, image_url):
-        if ctx.message.author.id == self.bot.config['discord']['owner_client_id']:
+        if ctx.message.author.id == str(self.bot.config['discord']['owner_client_id']):
             try:
                 r = requests.get(image_url)
 
@@ -37,7 +37,7 @@ class Core:
             except discord.errors.InvalidArgument:
                 await self.bot.say_delete("Not a valid avatar.")
         else:
-            await self.bot.say_delete("You are not the self.bot owner!")
+            await self.bot.say_delete("You are not the bot owner!")
 
     @commands.command(
         name="setplaying",
@@ -60,6 +60,31 @@ class Core:
     async def get_invite_link(self):
         await self.bot.say("https://discordapp.com/oauth2/authorize?client_id={}&scope=b;ot&permissions=0".format(
             self.bot.config['discord']['bot_client_id']))
+
+    @commands.command(
+        name="clean",
+        aliases=["purge"],
+        description="Clear this bot's messages from the chat history.",
+        brief="Clean the chat log.",
+        pass_context=True
+    )
+    async def clean(self, ctx):
+        # async def clean(self, ctx, clear_bot=True):
+        def is_bot(message):
+            return message.author == ctx.message.server.me
+
+        # def is_author(message):
+        #     return message.author == ctx.message.author
+        #
+        # if clear_bot:
+        #     deleted = await self.bot.purge_from(ctx.message.channel, limit=1000, check=is_bot)
+        # else:
+        #     deleted = await self.bot.purge_from(ctx.message.channel, limit=1000, check=is_author)
+
+        deleted = await self.bot.purge_from(ctx.message.channel, limit=1000, check=is_bot)
+
+        await self.bot.say("Deleted {} message(s).".format(len(deleted)))
+
 
 def setup(bot):
     bot.add_cog(Core(bot))
